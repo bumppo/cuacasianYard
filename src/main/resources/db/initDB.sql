496d6cd636dfbb36370f995a67e1b229abffd5f6
VitalyMCMXC <vitaly.moskalik@gmail.com>
2016-02-15 16:32:42 +0300

    initial commit

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100;
+
+CREATE TABLE users
+(
+  id    INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name  VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE meals
+(
+  id      INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name    VARCHAR NOT NULL,
+  cost    INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,18 @@
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100;
+
+INSERT INTO users (name) VALUES
+  ('Виталик'),
+  ('Алексей Аксенов'),
+  ('Алексей Хныкин'),
+  ('Диана'),
+  ('Шамиль'),
+  ('Макс');
+
+INSERT INTO meals (name, cost, user_id) VALUES
+  ('виски', 500, 100),
+  ('пиво', 300, 101),
+  ('ром', 500, 102),
+  ('сок', 150, 103),
+  ('сидр', 350, 104),
+  ('водка', 200, 105);
